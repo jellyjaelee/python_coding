2757c04d107b095c5bfbdd34e8575cd4ad84b61d
Jae Lee <jayjhlee90@gmail.com>
2018-08-20 20:46:31 -0700

    Initial Commit

---
+++ fibonacci_recursion.py
@@ -0,0 +1,9 @@
+def get_fibonacci(value):
+    if value == 0 :
+        return 0
+    if value == 1:
+        return 1
+
+    return get_fibonacci(value-1) + get_fibonacci(value - 2)
+
+print (get_fibonacci(100))
